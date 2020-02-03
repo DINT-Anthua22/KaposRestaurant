2a3cb5991719955d14e299dc48e486d36d51bf9a
Jorge <jorge.llinares@iesdoctorbalmis.com>
2020-02-03 14:48:09 +0100

    Añadido PropertyChangedModelo.cs

---
+++ KaposRestaurant/Model/PropertyChangedModelo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaposRestaurant.Model
+{
+    public partial class CATEGORIA : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    public partial class ELEMENTO : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    public partial class FACTURA : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    public partial class COMANDA : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
