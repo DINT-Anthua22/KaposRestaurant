9ab8b2493dfa7c9be6e6213a193939bc49f157c4
albertorodriguez1998 <alberto.rodriguezruiz@iesdoctorbalmis.com>
2020-02-05 09:51:26 +0100

    Mantenimiento de categorías terminado

--- KaposRestaurant/Services/BlobStorage.cs
+++ KaposRestaurant/Services/BlobStorage.cs
@@ -3,6 +3,7 @@ using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,23 +13,18 @@ namespace KaposRestaurant.Services
 {
     public static class BlobStorage
     {
-        private static readonly CloudStorageAccount storageacc;
-        private static readonly StorageCredentials credentials;
-        private static readonly CloudBlobClient blobClient;
-
-        static BlobStorage()
+        public static string guardarImagen(string filename, string blobReference)
         {
-            credentials = new StorageCredentials("proyectoblobstorage", "x6q/bmEQExsn29HDqfq2TeqH4u3Yb8La68zmm3Udol58TlRK8COaNMRxdXnrcx5g11PuIzAwi+z14m/eUq6H2Q==");
-            storageacc = new CloudStorageAccount(credentials, false);
-            blobClient = storageacc.CreateCloudBlobClient();
-        }
+            StorageCredentials credentials = new StorageCredentials("proyectoblobstorage", "x6q/bmEQExsn29HDqfq2TeqH4u3Yb8La68zmm3Udol58TlRK8COaNMRxdXnrcx5g11PuIzAwi+z14m/eUq6H2Q==");
+            CloudStorageAccount storageacc = new CloudStorageAccount(credentials, true);
 
-        public static void guardarImagen(string filename)
-        {
-            CloudBlobContainer container = blobClient.GetContainerReference("democontainer");
+            CloudBlobClient blobClient = storageacc.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("imagenesproyecto");
 
-            container.CreateIfNotExists();
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference("DemoBlob");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobReference);
+            blockBlob.UploadFromFile(filename);
+
+            return blockBlob.Uri.AbsoluteUri;
         }
 
 
