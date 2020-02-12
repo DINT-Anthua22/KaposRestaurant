0c6d70ac1ddd84a2c1ff9bd6de75bd046baabf5d
Anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2020-02-12 10:14:38 +0100

    Agrego un temporizador para que se actualice el datagrid cada 2 segundos

--- KaposRestaurant/ViewModel/ConsultarPedidoModel.cs
+++ KaposRestaurant/ViewModel/ConsultarPedidoModel.cs
@@ -19,24 +19,26 @@ namespace KaposRestaurant.ViewModel
 
         public ObservableCollection<COMANDA> Comandas { get; set; }
 
-        private DispatcherTimer timer;
+        
 
         public double Precio { get; set; }
         public ConsultarPedidoViewModel()
         {
             Comandas = new ObservableCollection<COMANDA>(BbddService.GetComandas().OrderByDescending(x => x.IdComanda).ToList());
-           
+            ActualizarDataGrid();
+
         }
 
         private void ActualizarDataGrid()
         {
-            
-        }
-
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(10);
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += TimerControlador;
+            timer.Start();
+        }
+        private void TimerControlador(object sender, EventArgs e)
+        {
+           Comandas = new ObservableCollection<COMANDA>(BbddService.GetComandas().OrderByDescending(x => x.IdComanda).ToList());
 
         }
         public void Actualizar()
